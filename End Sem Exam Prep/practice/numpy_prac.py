dd4d24d7122fdfeadc1736671db6b60624be6191
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-09 09:35:19 +0530

    Practiced files added

--- End Sem Exam Prep/practice/numpy_prac.py
+++ End Sem Exam Prep/practice/numpy_prac.py
@@ -1,7 +1,9 @@
 import numpy as np
 '''arr=np.array([1,2,3,4])
-print(arr)'''
+print(arr)
 arr2=np.zeros((2,2))
 print(arr2)
 arr3=np.ones((2,2))
-print(arr3)
+print(arr3)'''
+arr4=np.eye(3)
+print(arr4)
