77e955eb1c60384cbd1cb51a504a80527d0b609f
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-09 09:42:52 +0530

    Practiced files added

--- End Sem Exam Prep/practice/numpy_prac.py
+++ End Sem Exam Prep/practice/numpy_prac.py
@@ -11,3 +11,8 @@ print(arr4)
 arr5=np.empty((2,2))
 print(arr5)'''
 # Array Operation Functions
+arr=np.array([1,2,3,4])
+print(np.max(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.sort(arr)) # Default is ascending 
