b76d15769f257ac7c37ae71282536eb594bb5703
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-09 10:23:20 +0530

    Practiced files added

--- End Sem Exam Prep/practice/pandas_prac.py
+++ End Sem Exam Prep/practice/pandas_prac.py
@@ -1,3 +1,6 @@
 import pandas as pd
-a=pd.Series([10,20,30])
-print(a)
+'''a=pd.Series([10,20,30])
+print(a)'''
+data={"Name":["Sanjay","Kumar","S"], "Age":[20,21,20]}
+d=pd.DataFrame(data)
+print(d)
