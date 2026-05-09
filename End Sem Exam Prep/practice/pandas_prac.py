fe98e10f1109fa7aecc2112c971cc1d66f83ff26
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-09 10:27:27 +0530

    Practiced files added

--- End Sem Exam Prep/practice/pandas_prac.py
+++ End Sem Exam Prep/practice/pandas_prac.py
@@ -1,6 +1,7 @@
 import pandas as pd
 '''a=pd.Series([10,20,30])
 print(a)'''
-data={"Name":["Sanjay","Kumar","S"], "Age":[20,21,20]}
-d=pd.DataFrame(data)
-print(d)
+data={"Name":["Sanjay","Kumar","S"], "Age":[20,21,20]} # We have normal dictionary type here
+d=pd.DataFrame(data) # Where we use dataframe to structure it proeprly
+print("The Modified Data \n ",d)
+print("The UnModified Data \n",data)
