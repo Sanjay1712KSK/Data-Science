dda994322ef70ff7f086ee7ac48cc1172445dead
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-09 09:37:35 +0530

    Practiced files added

--- End Sem Exam Prep/practice/numpy_prac.py
+++ End Sem Exam Prep/practice/numpy_prac.py
@@ -1,4 +1,5 @@
 import numpy as np
+# Array Creation Functions
 '''arr=np.array([1,2,3,4])
 print(arr)
 arr2=np.zeros((2,2))
@@ -6,6 +7,7 @@ print(arr2)
 arr3=np.ones((2,2))
 print(arr3)
 arr4=np.eye(3)
-print(arr4)'''
+print(arr4)
 arr5=np.empty((2,2))
-print(arr5)
+print(arr5)'''
+# Array Operation Functions
