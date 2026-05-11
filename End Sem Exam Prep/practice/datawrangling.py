3f4a5bdc5930bec01b053da41da29ca093a03743
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-11 19:34:30 +0530

    PYQs coding questions init

---
+++ End Sem Exam Prep/practice/datawrangling.py
@@ -0,0 +1,5 @@
+'''questions from - End Sem Exam Prep/PYQs/21CSS303T 08.07.2024.pdf (15 marks)
+                    End Sem Exam Prep/PYQs/21CSS303T 10.12.2025 AN.pdf
+                    End Sem Exam Prep/PYQs/21CSS303T 13.05.2024 AN.pdf (String manipulation)
+                    End Sem Exam Prep/PYQs/21CSS303T 14.07.2025 AN.pdf
+                    End Sem Exam Prep/PYQs/21CSS303T 17.05.2025 AN.pdf'''
--- End Sem Exam Prep/practice/webapi.py
+++ End Sem Exam Prep/practice/webapi.py
@@ -3,3 +3,6 @@ url="https://jsonplaceholder.typicode.com/todos/1"
 res=r.get(url)
 data=res.json()
 print(data)
+'''question from - End Sem Exam Prep/PYQs/21CSS303T 08.07.2024.pdf
+                  End Sem Exam Prep/PYQs/21CSS303T 13.05.2024 AN.pdf
+                  End Sem Exam Prep/PYQs/21CSS303T 14.07.2025 AN.pdf'''
