3d6236b7da4a2a45717139c32e98e2fc9f2d1836
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-11 20:10:57 +0530

    data wrangling practice code

--- End Sem Exam Prep/practice/datawranglingprac.py
+++ End Sem Exam Prep/practice/datawranglingprac.py
@@ -79,3 +79,32 @@ scaler = StandardScaler()
 df["Marks"] = scaler.fit_transform(df[["Marks"]])
 
 print(df)
+
+'''STEP 8 — Outlier Handling
+
+Outliers are abnormal extreme values.
+
+Example:
+
+500
+
+in Marks column.
+
+Detect Outliers Using IQR
+
+Formula:
+
+IQR=Q3-Q1'''
+
+
+Q1 = df["Marks"].quantile(0.25)
+Q3 = df["Marks"].quantile(0.75)
+
+IQR = Q3 - Q1
+
+lower = Q1 - 1.5 * IQR
+upper = Q3 + 1.5 * IQR
+
+df = df[(df["Marks"] >= lower) & (df["Marks"] <= upper)]
+
+print(df)
