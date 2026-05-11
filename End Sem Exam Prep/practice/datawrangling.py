91fe2ed5ccd0dd9fdf33ebc4e14ace2b295cd1bd
Sanjay1712KSK <sanjayksk1712@gmail.com>
2026-05-11 20:30:38 +0530

    data wrangling PYQs code

--- End Sem Exam Prep/practice/datawrangling.py
+++ End Sem Exam Prep/practice/datawrangling.py
@@ -3,3 +3,25 @@
                     End Sem Exam Prep/PYQs/21CSS303T 13.05.2024 AN.pdf (String manipulation)
                     End Sem Exam Prep/PYQs/21CSS303T 14.07.2025 AN.pdf
                     End Sem Exam Prep/PYQs/21CSS303T 17.05.2025 AN.pdf'''
+
+''' Healthcare organization question
+They maintain : 
+    1. Electronic medical records
+    2. missing values in crtical fields such as age and 
+        inconsistent categorical entries in diagnosis 
+        and extreme outliers in test scores
+
+To-Do:
+    1. Structured data wrangling and cleaning process
+    2. strategies for handling missing data
+    3. correction of inconsistencies
+    4. managing outlier (by IQR , Z-Score)
+'''
+import pandas as pd
+import numpy as np
+p=pd.read_csv("healthcare.csv")
+# Data cleaning
+p["Name"]=p["Name"].str.strip() 
+# Handling missing values
+p["Age"]=p["Age"].fillna(p["Age"].mean())
+p["Gender"]=p["Gender"].fillna("Unknown")
